aba24564ce373ebc1d31b6da36c4c585487235ad
VishalDnyandeoPatil <vp1610858@gmail.com>
2024-01-03 01:35:41 +0530

    Todo app User's login is done

--- routes/user_route.js
+++ routes/user_route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt= require('bcrypt');
-
+require('dotenv').config();
 const {users} = require("../model/user_model");
 const {auth} = require("../middelware/authentication")
 
@@ -27,5 +27,29 @@ userRoute.post("/signup", async(req,res)=>{
     } 
     catch (error) {
         res.send({msg:error.message});
+    }
+});
+
+userRoute.post("/login", async(req,res)=>{
+    try {
+       const payload = req.body;
+       const User =  await users.findOne({email:payload.email});
+       if(!User){
+        return res.send("Please signup")
+       }
+       const correctPass = await bcrypt.compareSync( payload.password,User.password);
+       if(correctPass){
+        const token = await jwt.sign({ email:User.email,userId:User._id}, process.env.secretKey, { expiresIn: '2m' });
+        
+        const refreshtoken = await jwt.sign({ email:User.email,userId:User._id}, process.env.refreshsecretKey, { expiresIn: '3m' });
+        res.json({msg:"Login Sucessful",token, refreshtoken})
+       }
+       else{
+        res.send({msg:"Invalid credentials"})
+       }
+    } 
+    catch (error) {
+        console.log("Error at user route");
+        res.send(error.message);
     }
 });
