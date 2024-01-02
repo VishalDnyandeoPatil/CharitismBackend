4ef686ff0c2fadb14b32d0418a139923b937b4a5
VishalDnyandeoPatil <vp1610858@gmail.com>
2024-01-03 02:16:26 +0530

    Todo app todo route's get route is done

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -1,3 +1,29 @@
 const express = require('express');
 const {todo}= require('../model/todoModel');
-const{Auther}= require('../middelware/authorization')
+const{Auther}= require('../middelware/authorization');
+const todoRoute= express.Router();
+
+todoRoute.get("/",Auther(["User","Super Admin","Admin"]),async(req,res)=>{
+
+    try {
+        const {userId}= req.body;
+        const Todo = await todo.find({$and:[{userId}]});
+        res.json({msg:"Your post",Todo});    
+    } 
+    catch (error) {
+        res.send(error.message);
+    }
+
+
+});
+
+todoRoute.get('/:id',Auther(["Super Admin","Admin"]),async(req,res)=>{
+    try {
+        const id = req.params.id;
+        const Todo = await todo.findById(id);
+        res.send({Todo})    
+    } 
+    catch (error) {
+        res.send({msg:error.message})
+    }
+ })
