2a8368da1dfd76aa639766247d9c82d807f2d340
VishalDnyandeoPatil <vp1610858@gmail.com>
2024-01-03 00:51:01 +0530

    App connection to db is done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const express = require('express');
+const {connection}= require('./config/db');
+
+
+const app = express();
+app.use(express.json());
+
+
+
+app.listen(process.env.PORT,async()=>{
+    try {
+        await connection;
+        console.log("Todo app connected to DB");
+    } 
+    catch (error) {
+        console.log(error.message);    
+    }
+})
